60b621d4ef454ec24455f5a844638696d8399721
ruhan-2908 <ruhankb29@gmail.com>
2026-02-02 19:05:00 +0530

    Project Structure

---
+++ src/main/java/com/tally/Tally/TallyApplication.java
@@ -0,0 +1,13 @@
+package com.tally.Tally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tally/Tally/TallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.Tally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

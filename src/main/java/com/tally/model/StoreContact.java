595e6e06c39ce9e6fb96565a2f92e93f6199a7f2
ruhan-2908 <ruhankb29@gmail.com>
2026-02-09 23:03:41 +0530

    Enable proper relship between User and Store

--- src/main/java/com/tally/model/StoreContact.java
+++ src/main/java/com/tally/model/StoreContact.java
@@ -3,10 +3,12 @@ package com.tally.model;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Email;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Embeddable
+@Builder
 public class StoreContact {
     private String address;
     private String phone;

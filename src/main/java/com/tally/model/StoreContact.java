2d83fbdb54ffbd0d298c7c1d76e9447c7b7d463c
ruhan-2908 <ruhankb29@gmail.com>
2026-02-09 23:09:00 +0530

    Fix Hibernate Runtime Error

--- src/main/java/com/tally/model/StoreContact.java
+++ src/main/java/com/tally/model/StoreContact.java
@@ -3,11 +3,15 @@ package com.tally.model;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class StoreContact {
     private String address;

64052357e4110db360a809e602de702bbd4620c5
ruhan-2908 <ruhankb29@gmail.com>
2026-02-18 22:26:44 +0530

    Scaffold Customer APIs

---
+++ src/main/java/com/tally/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.tally.repository;
+
+
+import com.tally.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+    List<Customer> findByFullNameContainingIgnoreCaseOrEmailContainingIgnoreCase(
+            String fullName,String email);
+
+}
